9dda3147ca54b224d6e669c08307d059c3783d78
Chris Otto <chris.otto6@gmail.com>
2020-06-18 12:41:19 -0500

    More updates

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withUrqlClient } from 'next-urql'
+import { AppProps } from 'next/app'
+import fetch from 'isomorphic-unfetch'
+
+const GRAPHQL_ENDPOINT = `http://localhost:3000/api/graphql`
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default withUrqlClient({ url: GRAPHQL_ENDPOINT, fetch })(App)
